397d1bbc4b473174003fb618a42fcb338b9ea7e7
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2021-04-18 19:59:49 -0300

    header feito com responsivo

--- praticando-meu-front-end/src/App.js
+++ praticando-meu-front-end/src/App.js
@@ -5,6 +5,7 @@ import Swal from 'sweetalert2'
 
 
 function App() {
+  
   function alerta(){
     Swal.fire('sejá bem vindo',
     "este é um projeto ainda em desenvolvimento e pode conter falhas, caso tenha alguma sugestão em como podemos melhorar mande um email para nós no final da pagina",
--- praticando-meu-front-end/src/components/Header.js
+++ praticando-meu-front-end/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import "./header.css"
+import Fundo from "../image/header.jpg"
 
 
 
@@ -9,16 +10,17 @@ export class Header extends Component {
     render() {
         return (
             <div className='header'>
-               
-                <h2>
-                    Melhore suas habilidades de codificação de front-end criando projetos reais
-                </h2>
-                <p>
-                        Resolva desafios reais de HTML, CSS e JavaScript enquanto trabalha com designs profissionais. Aqui você Encontrara diversos projetos de diferentes niveis para Praticar e melhorar ainda mais seu nivel de codificação
-                </p>
-               
-
-              
+               <div className='texto'>
+                    <h2>
+                            Melhore suas habilidades de codificação de front-end criando projetos reais
+                    </h2>
+                    <p>
+                            Resolva desafios reais de HTML, CSS e JavaScript enquanto trabalha com designs profissionais. Aqui você Encontrara diversos projetos de diferentes niveis para Praticar e melhorar ainda mais seu nivel de codificação
+                    </p>
+               </div>
+                <div className='image'>
+                  <img src={Fundo} alt=''/>
+                </div>
             </div>
         )
     }
