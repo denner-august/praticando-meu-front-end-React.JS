b516f06fa8304c424385304cf0c55aa62075be4a
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2021-04-20 20:34:52 -0300

    sessao playlist | reponsivo feito

--- praticando-meu-front-end/src/App.js
+++ praticando-meu-front-end/src/App.js
@@ -5,6 +5,7 @@ import React from 'react';
 import Nav from "./components/Nav"
 import Header from "./components/Header"
 import Desafio from "./components/Desafio"
+import Playlist from "./components/Playlist"
 
 
 function alerta(){
@@ -22,6 +23,7 @@ class App extends React.Component{
        <Nav/>
        <Header/>
        <Desafio/>
+       <Playlist/>
        
       </div>
     );
---
+++ praticando-meu-front-end/src/components/Playlist.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import "./playlist.css"
+
+import Cursoemvideo from "../image/playlist/cursoemvideo.jpg"
+import Flexbox from "../image/playlist/flexbox.jpg"
+import Cssgrid from "../image/playlist/grid.jpg"
+
+
+
+export class Playlist extends Component {
+   
+
+    render() {
+        return (
+            <div className='playlist desafio'>
+                <h1>Playlist para seu Desenvolvimento</h1>
+
+               <ul>
+                    <div>
+                            <img src={Cursoemvideo} alt='curso em video'/>
+                            <p>Se você deseja entender Logica de programação sem duvidas esse é a playlist certa</p>
+                            <a href="https://www.youtube.com/playlist?list=PLHz_AreHm4dmSj0MHol_aoNYCSGFqvfXV" target="_blank" rel="noopener noreferrer"><strong>Acessar</strong></a>
+                    </div>
+
+                    <div>
+                            <img src={Flexbox} alt='curso de flexbox'/>
+                            <p> Um dos melhores cursos Gratuitos que você vai encontrar sobre flexbox</p>
+                            <a href="https://youtu.be/qq0z-YSPJLI" target="_blank" rel="noopener noreferrer"><strong>Acessar</strong></a>
+                    </div>
+
+                    <div>
+                            <img src={Cssgrid} alt='curso de cssgrid'/>
+                            <p>Um dos melhores cursos Gratuitos que você vai encontrar sobre Grid layout</p>
+                            <a href="https://youtu.be/qq0z-YSPJLI" target="_blank" rel="noopener noreferrer"><strong>Acessar</strong></a>
+                    </div>
+                   
+               </ul>
+               
+            </div>
+        )
+    }
+}
+
+export default Playlist
