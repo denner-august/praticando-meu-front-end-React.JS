64b0b77bd13280055bd2220eb1356ab1414f5f70
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2021-04-22 16:06:11 -0300

    implementacao do email (funcionando corretamente)

--- praticando-meu-front-end/src/components/Footer.js
+++ praticando-meu-front-end/src/components/Footer.js
@@ -1,36 +1,68 @@
 import React, { Component } from 'react'
 
-//import Swal from 'sweetalert2'
+import Swal from 'sweetalert2'
 
 import "./footer.css"
 import Imageproframadora from "../image/footer/programadora.jpg"
 
 
 
-  
+
 
 
 export class Footer extends Component {
-    
-    
 
+    
     render() {
+
+        
+        function enviar(){
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                  toast.addEventListener('mouseenter', Swal.stopTimer)
+                  toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+              })
+              
+              Toast.fire({
+                icon: 'success',
+                title: 'Enviado com sucesso'
+              })
+
+              setInterval(() => {
+                InputenBranco()           
+              }, 1000); 
+        }
+
+        function InputenBranco(){
+            document.getElementById("name").value = ''
+            document.getElementById("email").value = ''
+            document.getElementById("message").value = ''
+          }
+
         return (
             <div className='footer'>
                 <img src={Imageproframadora} alt='mulher programando'/>
 
                <div>
-                    <form>
+                    <form onSubmit={enviar}  method="POST" action="https://formspree.io/f/mrgrzrdv"  >
                         <h1>Como podemos melhorar</h1>
-                        <input type='text' placeholder='Digite seu nome' required ></input>
-                        <input type='email' placeholder='Digite seu email' required ></input>
-                        <textarea placeholder='digite sua mensagem' required></textarea>
-                        <button type='submit' ><strong>Confirmar</strong></button>
+                        <input id='name' type='text' name="name" placeholder='Digite seu nome' required ></input>
+                        <input id='email'  name="email" type='email' placeholder='Digite seu email' required ></input>
+                        <textarea id='message' name="message" placeholder='digite sua mensagem' required></textarea>
+                        <button  type='submit'  ><strong>Confirmar</strong></button>
                     </form>
                </div>
             </div>
         )
     }
 }
+
+ 
 
 export default Footer
