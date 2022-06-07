bf655d1a03e5b62088ef0fb0abc7bb9a6470dcc4
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-06-07 17:59:27 -0300

    estilizando formulario

--- praticand-meu-front-end/src/components/footer/index.tsx
+++ praticand-meu-front-end/src/components/footer/index.tsx
@@ -1,10 +1,44 @@
+import { useState } from "react";
 import styles from "./styles.module.scss";
 
 export function Footer() {
+  const [email, seteMail] = useState(String);
+  const [mensagem, setMensagem] = useState(String);
+
+  function validation(e: { preventDefault: () => void }) {
+    e.preventDefault();
+  }
+
   return (
     <footer className={styles.container}>
       <div className={styles.backgroundIlustrator} />
-      <div className={styles.formulario}></div>
+      <div className={styles.formulario}>
+        <h2>Como podemos melhorar?</h2>
+        <div className={styles.card}>
+          <form method="post">
+            <label>Digite seu E-mail :</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              value={email}
+              onChange={(e) => seteMail(e.target.value)}
+            />
+            <label>Digite sua Mensagem :</label>
+            <textarea
+              rows={10}
+              cols={50}
+              required
+              value={mensagem}
+              onChange={(e) => setMensagem(e.target.value)}
+            />
+            <button onClick={validation} type="submit">
+              Enviar
+            </button>
+          </form>
+        </div>
+      </div>
     </footer>
   );
 }
