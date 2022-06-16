719bfc9efa7d1c0879ce3afc4ecc7ff13bf2433a
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-06-16 16:15:50 -0300

    criando tela de login

--- praticand-meu-front-end/src/components/login/index.tsx
+++ praticand-meu-front-end/src/components/login/index.tsx
@@ -1,3 +1,19 @@
+import styles from "./styles.module.scss";
+import Link from "next/link";
 export function Login() {
-  return <h1>login</h1>;
+  return (
+    <section className={styles.Container}>
+      <div className={styles.Login}>
+        <h3>Login</h3>
+        <form method="post" className={styles.formulario}>
+          <input type="email" placeholder="Digite seu email" required />
+          <input type="password" placeholder="Digite sua senha" required />
+          <button>Entrar</button>
+          <Link href="/">
+            <a>Criar conta</a>
+          </Link>
+        </form>
+      </div>
+    </section>
+  );
 }
