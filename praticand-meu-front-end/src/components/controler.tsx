db016b487cda23f4282cf5ef1e8a2ca8436fc74d
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-06-16 14:56:55 -0300

    component de login

--- praticand-meu-front-end/src/components/controler.tsx
+++ praticand-meu-front-end/src/components/controler.tsx
@@ -1,13 +1,19 @@
 import { Apresentacao } from "./apresentacao";
 import { Footer } from "./footer";
 import Header from "./Header";
+import { useState } from "react";
+import { Login } from "./login";
 
 export function Controler() {
-  return (
+  const [logado, setLogado] = useState(false);
+
+  return logado ? (
     <>
       <Header />
       <Apresentacao />
       <Footer />
     </>
+  ) : (
+    <Login />
   );
 }
